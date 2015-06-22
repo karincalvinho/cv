0c458e49aeb344493fa115d0f1291403d42ba598
Karin Calvinho <karincalvinho@gmail.com>
2015-06-21 23:46:28 -0400

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Filename,Tafel Slope (mV/dec),Potential at 0.1 mA/cm2 vs NHE (V),Potential at 10mA/cm2 (V),Exchange Current Density (mA/cm2),Intercept,R-value"
-find ./March2015/033015 -name "CV*.txt" | while read x
+find ./data/March2015/033015 -name "CV*.txt" | while read x
 do
   path=$(dirname "${x}")
   filename=$(basename "${x}")
-  python cvsgraph.py "$x"
+  python cvs.py "$x"
 done
